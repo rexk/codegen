e313f5a615e08e99708d0192fe08bca5868c9f76
rexk <myungsunrex@gmail.com>
2014-10-01 18:05:09 +0900

    added optimist

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var se = require('./lib/stackexchange');
+var optimist = require('optimist');
+var argv = optimist.argv;
 
-se.run(function (err) {
-  if (err) {
-    console.log('error');
-    console.log(err);
-    return;
-  }
-  console.log('done');
-});
+if (!argv.mod) {
+  console.log('requires module name');
+  process.exit(1);
+  return;
+}
+
+var mod = require('./lib/' + argv.mod);
+if (mod === undefined || mod === null) {
+  console.log('module ' + argv.mod + ' is not found');
+}
+
+if (mod) {
+  mod.run(function (err) {
+    if (err) {
+      console.log('error');
+      console.log(err);
+      return;
+    }
+    console.log('done');
+  });
+}
